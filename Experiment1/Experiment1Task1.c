2d6eb8126a408f6879e2ade40bb6c38f363ad2cf
MdHamzaTumbi <mdhamzatumbi@eng.rizvi.edu.in>
2024-10-14 17:54:55 +0530

    Add files via upload

---
+++ Experiment1/Experiment1Task1.c
@@ -0,0 +1,13 @@
+/*WAP to print your name, age, class, division and UIN on the screen. Use
+escape sequences to properly format the output.*/
+#include <stdio.h>
+
+int main()
+{
+    printf("Full Name\t: Mohamed Hamza Tumbi\n");
+    printf("Age  \t\t: 18\n");
+    printf("Class   \t: FY AIDS\n");
+    printf("Div   \t\t: A\n");
+    printf("UIN   \t\t: 241A015\n");
+    return 0;
+}
---
+++ Experiment1/Experiment1Task2.c
@@ -0,0 +1,19 @@
+/*Aim: WAP to get students PCM marks from the user and find the average. Use conditional
+operator to print if the student is eligible for admission. Eligibility criteria is 50% in
+PCM.*/
+
+#include <stdio.h>
+
+int main()
+{
+    int p,c,m,avg;
+    printf("Enter Physics Marks:\t");
+    scanf("%d",&p);
+    printf("Enter Maths Marks:\t");
+    scanf("%d",&m);
+    printf("Enter Chemistry Marks:\t");
+    scanf("%d",&c);
+    avg=(p+c+m)/3;
+    (avg>=50)?printf("You are eligible for admission"):printf("You are not eligible for admission");
+    return 0;
+}
